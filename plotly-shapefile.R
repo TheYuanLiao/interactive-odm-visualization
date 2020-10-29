42dd14c170d92a8e1b66c1624ee183304b678ca0
Yuan Liao <30661239+TheYuanLiao@users.noreply.github.com>
2020-10-29 14:46:25 +0100

    Save plotly to json

--- plotly-shapefile.R
+++ plotly-shapefile.R
@@ -1,8 +1,12 @@
 library(plotly)
 library(sf)
+library(jsonlite)
 
 zones <- st_transform(st_read('InteractiveODM/data/zones_subset.shp'), crs = 4326)
 p <- plot_ly(zones,
         text = ~paste("Zone ", zone),
         hoverinfo = "text")
-plotly_build(p)
+j <- plotly_json(p = p, jsonedit = interactive(), pretty = TRUE)
+
+exportJSON <- toJSON(j$x$data)
+write(exportJSON, "zones.json")
